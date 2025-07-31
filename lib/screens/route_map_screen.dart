6295771fb46bbdc2b090498fbc1c79b5518ef433
Phattarakorn Nalinbenchaphan <ackfort2547@gmail.com>
2025-07-31 19:53:33 +0700

    Add img

--- lib/screens/route_map_screen.dart
+++ lib/screens/route_map_screen.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 class RouteMapScreen extends StatelessWidget {
   const RouteMapScreen({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('เส้นทางเดินรถประจำทาง')),
-      body: Center(child: Text('เส้นทางเดินรถประจำทาง', style: TextStyle(fontSize: 24))),
+      body: InteractiveViewer(
+        panEnabled: true, // อนุญาตให้เลื่อนภาพ
+        minScale: 0.5, // ซูมออกได้น้อยสุด 50%
+        maxScale: 3.0, // ซูมเข้าได้มากสุด 300%
+        child: Center(
+          child: Image.asset(
+            'assets/images/PSU-Bus-Routes.jpg', // เปลี่ยนเป็น path รูปของคุณ
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
     );
   }
 }
